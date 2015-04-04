60981b27d9fabd49d00fea49fbc46392fbbccbb4
Andrew Patton <andrew@acusti.ca>
2015-04-03 21:31:09 -0400

    :name_badge: browser → driver

--- test/affixing-header-specs.js
+++ test/affixing-header-specs.js
@@ -11,7 +11,7 @@ require('colors');
 // Figure out best way to bundle module for distribution
 
 function runTests(browserName) {
-    var browser;
+    var driver;
 
     function setupDocument() {
         browserName = browserName || 'chrome';
@@ -23,7 +23,7 @@ function runTests(browserName) {
             if (process.env.TRAVIS_BRANCH) {
                 tags.push(process.env.TRAVIS_BRANCH);
             }
-    		browser = new webdriver.Builder()
+    		driver = new webdriver.Builder()
     		.usingServer('http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub')
     		.withCapabilities({
                 'tunnel-identifier' : process.env.TRAVIS_JOB_NUMBER,
@@ -35,12 +35,12 @@ function runTests(browserName) {
                 tags                : tags
     		}).build();
     	} else {
-    		browser = new webdriver.Builder()
+    		driver = new webdriver.Builder()
     		.forBrowser(browserName)
     		.build();
     	}
         console.log(('\n  Running tests for ' + browserName).cyan);
-    	return browser.get('http://localhost:3000/test/index.html');
+    	return driver.get('http://localhost:3000/test/index.html');
     }
 
     // Returns a function to pass to executeAsyncScript; returns an object to the promise callback with position and top css of the header element
@@ -79,7 +79,7 @@ function runTests(browserName) {
         });
 
     	beforeEach(function() {
-            return browser.executeAsyncScript(function() {
+            return driver.executeAsyncScript(function() {
                 arguments[arguments.length - 1](document.documentElement.scrollHeight);
             }).then(function(scrollHeight) {
                 pageHeight = scrollHeight;
@@ -88,27 +88,27 @@ function runTests(browserName) {
 
         afterEach(function() {
             // Reset position and refresh browser, wait until it is reloaded
-            browser.manage().timeouts().setScriptTimeout(2000, 1);
-            browser.executeAsyncScript(scrollTo(), 0).then(function() {
-                browser.navigate().refresh();
+            driver.manage().timeouts().setScriptTimeout(2000, 1);
+            driver.executeAsyncScript(scrollTo(), 0).then(function() {
+                driver.navigate().refresh();
             });
-            browser.manage().timeouts().implicitlyWait(6000, 1);
-            return browser.wait(webdriver.until.elementLocated({className: 'is-ready'}));
+            driver.manage().timeouts().implicitlyWait(6000, 1);
+            return driver.wait(webdriver.until.elementLocated({className: 'is-ready'}));
         });
 
         after(function() {
-            return browser.quit();
+            return driver.quit();
             // Resolve promise
             // deferred.resolve();
         });
 
     	it('keeps header at top of document.body (off screen) when user scrolls down', function() {
-            var header = browser.findElement({className: 'affixing-header'});
+            var header = driver.findElement({className: 'affixing-header'});
             expect(header.getCssValue('top')).to.eventually.equal('0px');
             expect(header.getCssValue('position')).to.eventually.equal('absolute');
 
-            browser.manage().timeouts().setScriptTimeout(2000, 1);
-            return browser.executeAsyncScript(scrollTo(), Math.round(pageHeight / 2)).then(function(computedStyles) {
+            driver.manage().timeouts().setScriptTimeout(2000, 1);
+            return driver.executeAsyncScript(scrollTo(), Math.round(pageHeight / 2)).then(function(computedStyles) {
                 expect(computedStyles.top).to.equal('0px');
                 expect(computedStyles.position).to.equal('absolute');
             });
@@ -118,17 +118,17 @@ function runTests(browserName) {
             var scrollCount = 8,
                 scrollY     = Math.round(pageHeight / 2);
 
-            browser.manage().timeouts().setScriptTimeout(2000, 1 + scrollCount + 1);
-            browser.executeAsyncScript(scrollTo(), scrollY).then(function(computedStyles) {
+            driver.manage().timeouts().setScriptTimeout(2000, 1 + scrollCount + 1);
+            driver.executeAsyncScript(scrollTo(), scrollY).then(function(computedStyles) {
                 expect(computedStyles.top).to.equal('0px');
                 expect(computedStyles.position).to.equal('absolute');
             });
 
             while (scrollCount--) {
                 scrollY -= 4;
-                browser.executeAsyncScript(scrollTo(), scrollY);
+                driver.executeAsyncScript(scrollTo(), scrollY);
             }
-            return browser.executeAsyncScript(scrollTo(), scrollY - 2).then(function(computedStyles) {
+            return driver.executeAsyncScript(scrollTo(), scrollY - 2).then(function(computedStyles) {
                 // TODO: should this be more specific?
                 expect(computedStyles.top).to.not.equal('0px');
                 expect(computedStyles.position).to.equal('absolute');
@@ -140,17 +140,17 @@ function runTests(browserName) {
                 scrollY     = Math.round(pageHeight / 2),
                 scrollDelta = Math.round(scrollY / (scrollCount + 4));
 
-            browser.manage().timeouts().setScriptTimeout(2000, 1 + scrollCount + 1);
-            browser.executeAsyncScript(scrollTo(), scrollY).then(function(computedStyles) {
+            driver.manage().timeouts().setScriptTimeout(2000, 1 + scrollCount + 1);
+            driver.executeAsyncScript(scrollTo(), scrollY).then(function(computedStyles) {
                 expect(computedStyles.top).to.equal('0px');
                 expect(computedStyles.position).to.equal('absolute');
             });
 
             while (scrollCount--) {
                 scrollY -= scrollDelta;
-                browser.executeAsyncScript(scrollTo(), scrollY);
+                driver.executeAsyncScript(scrollTo(), scrollY);
             }
-            return browser.executeAsyncScript(scrollTo(), scrollY - scrollDelta).then(function(computedStyles) {
+            return driver.executeAsyncScript(scrollTo(), scrollY - scrollDelta).then(function(computedStyles) {
                 expect(computedStyles.top).to.equal('0px');
                 expect(computedStyles.position).to.equal('fixed');
             });
@@ -159,23 +159,23 @@ function runTests(browserName) {
     	it('allows header to disappear again when scrolling down', function() {
             var scrollY = Math.round(pageHeight / 2);
 
-            browser.manage().timeouts().setScriptTimeout(2000, 4);
-            browser.executeAsyncScript(scrollTo(), scrollY).then(function(computedStyles) {
+            driver.manage().timeouts().setScriptTimeout(2000, 4);
+            driver.executeAsyncScript(scrollTo(), scrollY).then(function(computedStyles) {
                 expect(computedStyles.top).to.equal('0px');
                 expect(computedStyles.position).to.equal('absolute');
             });
 
             // Scroll back up a bunch
             scrollY -= Math.round(pageHeight / 8);
-            browser.executeAsyncScript(scrollTo(), scrollY).then(function() {});
+            driver.executeAsyncScript(scrollTo(), scrollY).then(function() {});
             scrollY -= Math.round(pageHeight / 8);
-            browser.executeAsyncScript(scrollTo(), scrollY).then(function(computedStyles) {
+            driver.executeAsyncScript(scrollTo(), scrollY).then(function(computedStyles) {
                 // Header should be affixed
                 expect(computedStyles.top).to.equal('0px');
                 expect(computedStyles.position).to.equal('fixed');
             });
 
-            return browser.executeAsyncScript(scrollTo(), scrollY + 5).then(function(computedStyles) {
+            return driver.executeAsyncScript(scrollTo(), scrollY + 5).then(function(computedStyles) {
                 // Header should no longer be fixed
                 expect(computedStyles.top).to.not.equal('0px');
                 expect(computedStyles.position).to.equal('absolute');
