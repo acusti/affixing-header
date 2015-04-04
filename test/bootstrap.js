ff66fa9e5c341231bcd00b837503f71d9dfdcbd6
Andrew Patton <andrew@acusti.ca>
2015-04-03 21:54:50 -0400

    Ixnay on the peraoay

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -26,8 +26,7 @@ var runTests = require('./affixing-header-specs'),
 if (process.env.TRAVIS_JOB_NUMBER) {
 	browsers.push(
         {name: 'internet explorer'},
-        {name: 'safari', version: 7},
-        {name: 'opera'}/*,
+        {name: 'safari', version: 7}/*,
         {name: 'ipad', version: 8},
         {name: 'iphone', version: 8}*/
     );
