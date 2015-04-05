1282fddaf65cffe5a1be0d549d70c570fcf5a128
Andrew Patton <andrew@acusti.ca>
2015-04-05 14:17:35 -0400

    Add iPad to saucelabs browsers for tests

--- test/conductor.js
+++ test/conductor.js
@@ -10,8 +10,8 @@ var runTests  = require('./affixing-header-specs'),
 if (process.env.TRAVIS_JOB_NUMBER) {
 	browsers.push(
         {name: 'internet explorer'},
-        {name: 'safari', version: 7}/*,
-        {name: 'ipad', version: 8},
+        {name: 'safari', version: 7},
+        {name: 'ipad', version: 8}/*,
         {name: 'iphone', version: 8}*/
     );
 }
