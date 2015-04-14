356597b10971379c2a80d1bdf4e551648ad810dc
Andrew Patton <andrew@acusti.ca>
2015-04-14 12:25:36 -0400

    Use `version` not `platformVersion` for iOS appium config

--- test/conductor.js
+++ test/conductor.js
@@ -10,8 +10,8 @@ var runTests  = require('./affixing-header-specs'),
 if (process.env.TRAVIS_JOB_NUMBER) {
 	browsers.push(
         {browserName: 'safari', version: 7},
-        {browserName: 'ipad',   platformVersion: '8.2', appiumVersion: '1.3.7'},
-        {browserName: 'iphone', platformVersion: '8.2', appiumVersion: '1.3.7'},
+        {browserName: 'ipad',   version: '8.2', appiumVersion: '1.3.7'},
+        {browserName: 'iphone', version: '8.2', appiumVersion: '1.3.7'},
         {browserName: 'chrome', platformVersion: '4.0', platformName: 'Android', appiumVersion: '1.3.7', deviceName: 'Android Emulator'},
         {browserName: 'internet explorer'}
 	);
