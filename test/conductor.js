2ce2a9f0ca9c43ff3f42d86c56688b88549f1a91
Andrew Patton <andrew@acusti.ca>
2015-04-15 10:27:30 -0400

    :arrow_down: Test with iOS 7.1.2

--- test/conductor.js
+++ test/conductor.js
@@ -10,8 +10,8 @@ var runTests  = require('./affixing-header-specs'),
 if (process.env.TRAVIS_JOB_NUMBER) {
 	browsers.push(
         {browserName: 'safari', version: 7},
-        {browserName: 'ipad',   version: '8.2', appiumVersion: '1.3.7'},
-        {browserName: 'iphone', version: '8.2', appiumVersion: '1.3.7'},
+        {browserName: 'ipad',   version: '7.1.2', appiumVersion: '1.3.7'},
+        {browserName: 'iphone', version: '7.1.2', appiumVersion: '1.3.7'},
         {browserName: 'chrome', platformVersion: '4.0', platformName: 'Android', appiumVersion: '1.3.7', deviceName: 'Android Emulator'},
         {browserName: 'internet explorer'}
 	);
