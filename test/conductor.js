d53c89156280ebe25ee057744d722783eb00790f
Andrew Patton <andrew@acusti.ca>
2015-04-15 12:10:21 -0400

    Yet another desired capabilities config
    This time inspired by
    https://github.com/appium/appium/blob/9038dba0ca4b4e27629d215e540c6210f8
    ef6e2e/lib/devices/ios/ios.js#L14

--- test/conductor.js
+++ test/conductor.js
@@ -10,8 +10,8 @@ var runTests  = require('./affixing-header-specs'),
 if (process.env.TRAVIS_JOB_NUMBER) {
 	browsers.push(
         {browserName: 'safari', version: '7'},
-        {browserName: 'safari', deviceName: 'iPad 2',           platformVersion: '8.3', platformName: 'iOS', appiumVersion: '1.3.7'},
-        {browserName: 'safari', deviceName: 'iPhone 6',         platformVersion: '8.3', platformName: 'iOS', appiumVersion: '1.3.7'},
+        {browserName: 'safari', deviceName: 'iPhone Simulator', platformVersion: '7.1', platformName: 'iOS', appiumVersion: '1.3.7'},
+        {browserName: 'safari', deviceName: 'iPad Simulator',   platformVersion: '7.1', platformName: 'iOS', appiumVersion: '1.3.7'},
         {browserName: 'chrome', deviceName: 'Android Emulator', platformVersion: '4.3', platformName: 'Android', appiumVersion: '1.3.7'},
         {browserName: 'internet explorer'}
 	);
